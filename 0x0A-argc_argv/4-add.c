eac47b58f36ca68813f8083ae4ee8bf3122cfd05
Whogesyou <obazzdaman@gmail.com>
2022-11-07 14:30:48 -0800

    updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,17 +14,16 @@ int main(int argc, char *argv[])
 
 	for (num1 = 1; num1 < argc; num1++)
 	{
-		for (num2 = 0; argv[num1][num2] != '\O'; num2++)
+		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 		{
 			if (!isdigit(argv[num1][num2]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-
 		}
 		sum = sum + atoi(argv[num1]);
-					}
-					printf("%d\n", sum);
-					return (0);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
